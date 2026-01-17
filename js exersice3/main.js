a424a76341e8889acb36681a89310fe451f203a1
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-17 17:44:25 +0300

    submited ex3

---
+++ js exersice3/main.js
@@ -0,0 +1,11 @@
+let isSunny = true
+let isWeekend = true
+
+// oprator AND &&
+console.log(isSunny && isWeekend)
+// oprator OR ||
+console.log(isSunny || isWeekend)
+// oprator INVERT !
+console.log(!isSunny)
+// oprator INVERT !
+console.log(!isWeekend)
