5ae9340c21bec834271764180874b670bdcc59fd
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-21 12:01:43 +0300

    submited ex6

---
+++ js exersice6/main.js
@@ -0,0 +1,8 @@
+let colors = ["red","white","yellow","tomato","whitesmoke","blue","black"]
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+console.log(colors[3])
+console.log(colors[4])
+console.log(colors[5])
+console.log(colors[6])
