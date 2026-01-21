30a13fd10ccc0d8fcf735eae8d4b3b15e8d9dcdb
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-21 17:47:41 +0300

    submited ex8

---
+++ js exersice8/main.js
@@ -0,0 +1,11 @@
+let car = {
+    model : "juniper-model y", 
+    price : "$41630",
+    year : 2020
+}
+
+console.log("TESLA CAR INFO")
+
+console.log(car.model)
+console.log(car.price)
+console.log(car.year)
