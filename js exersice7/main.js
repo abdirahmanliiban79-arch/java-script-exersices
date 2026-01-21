342054f21cfe57817027ab4e7790939d10683751
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-21 14:25:47 +0300

    submited ex7

---
+++ js exersice7/main.js
@@ -0,0 +1,9 @@
+let colors = ["red","white","yellow","tomato","whitesmoke","blue","black"]
+
+colors.push("hurrudi")
+console.log(colors)
+
+colors.push()
+console.log(colors)
+
+console.log(colors.length)
