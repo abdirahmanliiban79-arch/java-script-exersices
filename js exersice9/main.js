e18c9f367ef2e20ad61f8dc9c295d1c1899c3fed
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-22 14:53:45 +0300

    submited ex9

---
+++ js exersice9/main.js
@@ -0,0 +1,16 @@
+let car = {
+    model : "juniper-model y", 
+    price : "$41630",
+    year : 2020,
+    start : function (){
+        console.log("this car has started on "
+        +this.year
+        )
+    }
+}
+
+console.log("TESLA CAR INFO")
+
+console.log(car.model)
+console.log(car.price)
+console.log(car.year)
