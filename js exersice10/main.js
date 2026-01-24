49dcf0afd18d6e87286132fee6f5f6d60add1566
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-24 15:26:29 +0300

    submited ex10

---
+++ js exersice10/main.js
@@ -0,0 +1,20 @@
+let library =[
+    {
+        Title : "Pride and projudice",
+        Author : "Jane Austen",
+        Year : 1813,
+    },
+    {
+        Title : "The Great Gasby",
+        Author : "F.Scott Fitzgerald",
+        Year : 1925,
+    },
+    {
+        Title : "1984",
+        Author : "George Orwell",
+        Year : 1949,
+    }
+]
+
+console.log(library[0].Title);
+console.log(library[1].name);
