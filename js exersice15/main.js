6732e6a2ef0fca80f9196412b2e05ccfbcbe3225
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-28 17:20:50 +0300

    submited ex15

---
+++ js exersice15/main.js
@@ -0,0 +1,33 @@
+
+let person1 ={   Name:"Ali",
+        Age : 25,
+        City : "mogadishu",
+    }
+
+let person2= {
+        Name : "sacdia",
+        Age : 20,
+        City : "puntland"
+    } 
+    
+let person3 = {
+        Name : "aj",
+        Age : 500,
+        City : "unknown",
+    }  
+    
+for (const key in person1){
+    console.log(key,":",person1[key])
+}  
+console.log("")  
+for (const key in person2){
+    console.log(key,":",person2[key])
+}   
+console.log("")   
+for (const key in person3){
+    console.log(key,":",person3[key])
+}    
+
+
+
+
