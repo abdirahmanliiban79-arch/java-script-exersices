beb6a80da6ab38cee3cecfc14bad7b52babcfb1f
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-01-29 15:28:16 +0300

    submited ex18

---
+++ js exersice18/main.js
@@ -0,0 +1,9 @@
+// WAY ONE
+let grade = 32
+const Result = grade <= 59 ? "FAIL" : "PASS";
+console.log(Result)
+
+// WAY 2
+let grades = 54
+const results =grades >= 60 ? "PASS" : "FAIL";
+console.log(results)
