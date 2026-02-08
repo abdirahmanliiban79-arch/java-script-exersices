33fd6541d25160affba30720b6a63c6b73e17b42
abdirahman liban <abdirahmanliban@Abdirahmans-MacBook-Air.local>
2026-02-08 18:05:57 +0300

    submited ex24

---
+++ js exersice24/main.js
@@ -0,0 +1,7 @@
+calculateArea = (width,height=width) => {
+    return `${width},${height}`
+} 
+// with value of height
+console.log(calculateArea(5*10))
+// without value of height
+console.log(calculateArea(5))
